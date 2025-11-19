9e134c1105311167823096dd9d0b54db97277c76
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-11-19 19:22:35 +0100

    Models creados (bien) y launch.json arreglado

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -2,17 +2,17 @@ public enum Nivel{Basico,Intermedio,Avanzado}
 public class Curso{
     public int Id { get; set; }
     public string Titulo { get; set; }
-    public string Descripccion { get; set; }
+    public string Descripcion { get; set; }
     public string Categoria { get; set; }
     public Nivel Nivel { get; set; }
     public DateTime Fecha_Creacion { get; set; }
 
     public Curso(){}
 
-    public Curso(string titulo, string descripccion, string categoria, Nivel nivel, DateTime fecha_Creacion)
+    public Curso(string titulo, string descripcion, string categoria, Nivel nivel, DateTime fecha_Creacion)
     {
         Titulo = titulo;
-        Descripccion = descripccion;
+        Descripcion = descripcion;
         Categoria = categoria;
         Nivel = nivel;
         Fecha_Creacion = fecha_Creacion;
