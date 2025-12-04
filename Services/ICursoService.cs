6fbe24be59dbf0532d69ad86c086db348645eb6d
Blancaclaire <clara.alonso.jimenez@gmail.com>
2025-12-04 20:55:54 +0100

    renombrado cursorepository

---
+++ Services/ICursoService.cs
@@ -0,0 +1,14 @@
+// using Models;
+
+// namespace CursosAPI.Repositories
+// {
+//     public interface ICursoRepository
+//     {
+//         Task<List<Curso>> GetAllAsync();
+//         Task<Curso?> GetByIdAsync(int id);
+//         Task AddAsync(Curso curso);
+//         Task UpdateAsync(Curso curso);
+//         Task DeleteAsync(int id);
+
+//     }
+// }
