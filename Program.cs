265a73245234ccf92268a8fb60c9ba00b6f71177
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-12-04 16:32:41 +0100

    Refactorizacion 2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,9 @@
-using CursosAPI.Controllers;
-using CursosAPI.Repositories;
-using CursosAPI.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("CursosDB");
 
-builder.Services.AddScoped<ICursoRepository, CursoRepository>();
-builder.Services.AddScoped<IInscripcionRepository, InscripcionRepository>();
-builder.Services.AddScoped<ILeccionRepository, LeccionRepository>();
-builder.Services.AddScoped<IPagoRepository, PagoRepository>();
-builder.Services.AddScoped<ICursoService, CursoService>();
-builder.Services.AddScoped<ILeccionService, LeccionService>();
-builder.Services.AddScoped<IInscripcionService, InscripcionService>();
-builder.Services.AddScoped<IPagoService, PagoService>();
-builder.Services.AddScoped<IReseñaRepository, ReseñaRepository>();
-builder.Services.AddScoped<IReseñaService, ReseñaService>();
-builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
