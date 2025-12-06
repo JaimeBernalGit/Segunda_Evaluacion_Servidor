98226bc3395e2721867b610a4ff282410361b9e5
Blancaclaire <clara.alonso.jimenez@gmail.com>
2025-12-06 17:31:56 +0100

    Añadidos metodos de filtro por path. Reseñas terminadas

--- Services/IResenaService.cs
+++ Services/IResenaService.cs
@@ -6,6 +6,9 @@ namespace CursosAPI.Services
     {
         Task<List<Resena>> GetAllAsync(int? calificacion, DateTime? fecha = null);
         Task<Resena?> GetByIdAsync(int id);
+        Task<List<Resena>> GetByUsuarioAndCursoAsync(int usuarioId, int cursoId);
+        Task<List<Resena>> GetByUsuarioAsync(int usuarioId);
+        Task<List<Resena>> GetByCursoAsync(int cursoId);
         Task AddAsync(ResenaCreateDTO resena);
         Task UpdateAsync(int id, ResenaUpdateDTO resena);
         Task DeleteAsync(int id);
